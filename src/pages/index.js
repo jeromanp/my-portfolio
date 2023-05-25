df106b1938c9bbebb2b9cc4840e692175c0676dc
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 11:56:09 -0500

    deleted font an image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl font-bold text-red-500">Jose Eduardo Roman</h1>
