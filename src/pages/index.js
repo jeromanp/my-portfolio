8c405874c905aef8d8d147e15e09306b977f1e97
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 13:08:49 -0500

    change css tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <Layout>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24`}
+        className={`flex min-h-screen flex-col items-center justify-between`}
       >
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <div className="z-10 w-full max-w-sm items-center justify-between font-mono text-sm lg:flex">
           <h1 className="text-4xl font-bold text-red-500">
             Jose Eduardo Roman
           </h1>
