f7b73220295264ab0427fd8fd913b0103bde9e88
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 18:43:59 -0500

    add title and description page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,13 @@
 import NavBar from "./NavBar";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
+     <Head>
+     <title>my portfolio</title>
+        <meta name="description" content="Webside Portfolio Jose Eduardo Roman"></meta>
+     </Head>
       <NavBar />
 
       <main className="container py-4">{children}</main>
