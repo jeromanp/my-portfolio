26afb24f95bcd1c4553071063a4b41aec8d22b10
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 18:40:02 -0500

    reverter changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,11 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>
-        <title>my portfolio</title>
-        <meta name="description" content="Jose Eduardo Roman Portfolio Webside"/>
-      </Head>
+    <Html>
+      <Head />
       <body>
         <Main />
         <NextScript />
