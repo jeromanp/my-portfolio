9726ddd052cbbe5a53b531d9d3c66b88edbcdbe8
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 12:11:36 -0500

    component navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+    return (
+      <nav className="pb-3 flex justify-end pr-5 pt-5">
+        <ul className="flex text-white text-lg font-semibold">
+          <li className="mr-3">Inicio</li>
+          <li className="mr-3">Sobre mí</li>
+          <li className="mr-3">Habilidades</li>
+          <li className="mr-3">Proyectos</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+    );
+  }
+  
