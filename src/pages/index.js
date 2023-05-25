763d70730babef817049b9334dcea9ab8f9d5868
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 12:11:11 -0500

    implement navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
+    <>
+    <NavBar />
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24`} >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl font-bold text-red-500">Jose Eduardo Roman</h1>
       </div>
     </main>
+    </>
   );
 }
