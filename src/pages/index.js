fc05bfaeb20a01497055baf20424e241eb4d6d60
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-25 12:47:25 -0500

    Import an use layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
-import NavBar from "@/components/NavBar";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
-    <>
-    <NavBar />
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24`} >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-bold text-red-500">Jose Eduardo Roman</h1>
-      </div>
-    </main>
-    </>
+    <Layout>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      >
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-4xl font-bold text-red-500">
+            Jose Eduardo Roman
+          </h1>
+        </div>
+      </main>
+    </Layout>
   );
 }
