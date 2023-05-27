6ba7fd8deba3495af8bd0bfd649dc62c9d55dac3
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-27 02:39:10 -0500

    add aboutMe

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,13 @@
 export const infoPersonal = {
-    name:"Jose Eduardo Roman",
-    professionOne: "Desarrollador Fullstack",
-    professionTwo:"Ingeniero en Telecomunicaciones, Sistemas y Electrónica",   
-}
+  name: "Jose Eduardo Roman",
+  professionOne: "Desarrollador Fullstack",
+  professionTwo: "Ingeniero en Telecomunicaciones, Sistemas y Electrónica",
+};
+
+export const aboutMe = {
+  intro: "Tengo un gusto por el conocimiento y por aprender cosas nuevas, me gusta leer artículos, noticias o publicaciones sobre temas que me interesan, tengo un gusto especial por la tecnología, astronomía, paleontología,historia, geografía y bilogía, lo que me ha llevado a ser una persona con conocimiento en diferentes áreas.",
+  estudio:  
+    "Estudié Ingeniería en Telecomunicaciones, Sistemas y Electrónica en la Universidad Nacional Autónoma de México, con especialidad en Comunicaciones. Cuento con dos diplomados en Redes e Interconectividad con tecnologías de Cisco (CCNA y CCNP).",
+  experiencia:
+    "Trabaje cerca de 2 años en una empresa proveedora de servicio de internet a nivel nacional, en donde me encargaba de la operación, instalación, revisión y mantenimiento de los equipos que proporcionaban el servicio de Video, Voz y Datos a sus suscriptores",
+};
