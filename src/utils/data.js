dc265ebb11e8b40cf85d8918e7760401645502d8
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-27 01:28:22 -0500

    data for page

---
+++ src/utils/data.js
@@ -0,0 +1,6 @@
+export const infoPersonal = {
+    name:"Jose Eduardo Roman",
+    professionOne: "Desarrollador Fullstack",
+    professionTwo:"Ingeniero en Telecomunicaciones, Sistemas y Electrónica",
+    aboutMe:" "
+}
