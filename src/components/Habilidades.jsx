dc05872997211fc46e5cb907dfa3d79f4b3c1aa9
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-04 03:23:50 -0500

    add some habilities

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -13,7 +13,9 @@ export default function Habilidades() {
               className="bg-slate-200 dark:bg-[#0a192f] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-500 items-center inline-block"
               key={i}
             >
-              <div className="p-1 items-center inline-block" /*style={{ width: '100px', height: '100px' }}*/>{hability.image}</div>
+              <div className="p-1 items-center inline-block" >
+              <img src={hability.image} alt={hability.skill} style={{ width: '200px', height: '200px' }}/>
+              </div>
               <p className="mt-4 text-xl pb-5">{hability.skill}</p>
             </div>
           ))}
