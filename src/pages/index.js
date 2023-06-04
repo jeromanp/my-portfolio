c4af5219845471bb847d13e3113013aae233a326
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-04 00:12:22 -0500

    divide section intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,20 @@
 import Layout from "@/components/Layout";
 import AboutMe from "@/components/AboutMe";
+import Intro from "@/components/Intro";
 import { infoPersonal } from "@/utils/data";
 
 export default function Home() {
   return (
-<Layout>
-  <main className="flex min-h-full flex-col items-center justify-between">
-    <section>
-      <div className="w-full max-w-sm items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold text-red-500">
-          {infoPersonal.name}
-        </h1>
-        <h3 className="text-center pt-4">
-          {infoPersonal.professionOne} & {infoPersonal.professionTwo}
-        </h3>
-        <img
-          src="/img-me.jpg"
-          alt="me"
-          className="h-50 rounded-full mt-4"
-        />
-      </div>
-    </section>
+    <Layout>
+      <main className="flex min-h-full flex-col items-center justify-between">
+        <section>
+          <Intro />
+        </section>
 
-    <section>
-      <AboutMe />
-    </section>
-  </main>
-</Layout>
-
+        <section>
+          <AboutMe />
+        </section>
+      </main>
+    </Layout>
   );
 }
