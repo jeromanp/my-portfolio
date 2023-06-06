020b3c1bff62d844d7785457316d0fb8c115d4dd
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-06 01:14:43 -0500

    add ID to section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,24 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <nav className="pb-4 flex justify-end pr-5 pt-5 bg-blue-400 flex">
       <ul className="flex text-white text-lg font-semibold self-center">
-        <li className="mr-5 hover:text-gray-900">Inicio</li>
-        <li className="mr-5 hover:text-gray-900">Sobre mí</li>
-        <li className="mr-5 hover:text-gray-900">Habilidades</li>
-        <li className="mr-5 hover:text-gray-900">Proyectos</li>
-        <li className="mr-5 hover:text-gray-900">Contacto</li>
+        <li className="mr-5 hover:text-gray-900">
+          <Link href="#intro">Inicio</Link>
+        </li>
+        <li className="mr-5 hover:text-gray-900">
+          <Link href="#about-me">Sobre mí</Link>
+        </li>
+        <li className="mr-5 hover:text-gray-900">
+          <Link href="#skils">Habilidades</Link>
+        </li>
+        <li className="mr-5 hover:text-gray-900">
+          <Link href="#proyects">Proyectos</Link>
+        </li>
+        <li className="mr-5 hover:text-gray-900">
+          <Link href="#contact">Contacto</Link>
+        </li>
       </ul>
     </nav>
   );
