35a894d2c132e668fefbb2ecb08413bccc07c5c0
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-06 00:59:02 -0500

    add style for text card

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -14,15 +14,19 @@ export default function Habilidades() {
               <div
                 className="bg-slate-200 dark:bg-[#0a192f] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-500 items-center inline-block"
                 key={i}
+                // style={{ width: "200px", height: "300px" }}
               >
                 <div className="p-1 items-center inline-block">
                   <img
                     src={hability.image}
                     alt={hability.skill}
-                    style={{ width: "150px", height: "150px" }}
+                    style={{ width: "90px", height: "90px" }}
                   />
                 </div>
-                <p className="mt-4 text-xl pb-5">{hability.skill}</p>
+                <p className="mt-4 text-xl pb-5 text-center">
+                  {hability.skill}
+                </p>{" "}
+                {/* Agrega text-center para centrar el texto */}
               </div>
             ))}
           </div>
