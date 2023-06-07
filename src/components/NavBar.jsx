aa67bf00b808af7cea8ef6728a9ce419595751cd
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-06 20:26:22 -0500

    border in nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="pb-4 flex justify-end pr-5 pt-5 bg-blue-400 fixed top-0 w-full">
-  <ul className="flex text-white text-lg font-semibold self-center">
-    <li className="mr-5 hover:text-gray-900">
+<nav className="pb-4 flex justify-end pr-5 pt-5 bg-custom-azul border border-custom-oro fixed top1 w-full">
+    <ul className="flex text-lg font-semibold self-center">
+    <li className="mr-5 hover:text-red-700">
       <Link href="#intro">Inicio</Link>
     </li>
-    <li className="mr-5 hover:text-gray-900">
+    <li className="mr-5 hover:text-red-700">
       <Link href="#about-me">Sobre mí</Link>
     </li>
-    <li className="mr-5 hover:text-gray-900">
+    <li className="mr-5 hover:text-red-700">
       <Link href="#skills">Habilidades</Link>
     </li>
-    <li className="mr-5 hover:text-gray-900">
+    <li className="mr-5 hover:text-red-700">
       <Link href="#proyects">Proyectos</Link>
     </li>
-    <li className="mr-5 hover:text-gray-900">
+    <li className="mr-5 hover:text-red-700">
       <Link href="#contact">Contacto</Link>
     </li>
   </ul>
