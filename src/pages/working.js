2ae0e98fadd3fe9e07625108bedf26d86fbc83f6
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-07 13:15:27 -0500

    add page working to deploy proyects

---
+++ src/pages/working.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import Process from '@/components/Process'
+
+export default function Working() {
+  return (
+      <main>
+      <Process />        
+      </main>
+  )
+}
