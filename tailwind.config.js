094e7170148f1246e51d3aaac78b9f76f533ab25
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-06 19:47:56 -0500

    add color of theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        custom: {
+          'oro': '#928652',
+          'azul': '#001844',
+
+        },
+      }
+      
     },
   },
   plugins: [],
 }
+
