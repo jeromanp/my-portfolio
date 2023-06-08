002f6b2d6f77f57824408a5e013907d6cfaada61
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-08 16:17:27 -0500

    add contact form

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
+import { contactoInfo } from "@/utils/data";
 
 export default function FormContact() {
   return (
-    <div>FormContact</div>
-  )
+    <div className="w-90 max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto">
+      <div className="p-20">
+        <h1 className="text-4xl font-bold mb-4 text-custom-oro">Contacto</h1>
+        <p className="mb-2 text-gray-500 text-justify text-xl">
+          {contactoInfo.parrafo_1}
+        </p>
+
+        <form className="form mx-auto mt-20">
+          <div className="flexy">
+            <label>
+              <input
+                required=""
+                placeholder="Nombre"
+                type="text"
+                className="input"
+              />
+            </label>
+          </div>
+
+          <label>
+            <input
+              required=""
+              placeholder="Email"
+              type="email"
+              className="input"
+            />
+          </label>
+
+          <label>
+            <input
+              required=""
+              type="tel"
+              placeholder="Número de contacto"
+              className="input"
+            />
+          </label>
+          <label>
+            <textarea
+              required=""
+              rows="3"
+              placeholder="Mensage"
+              className="input01"
+            ></textarea>
+          </label>
+
+          <button className="fancy" href="#">
+            <span className="top-key"></span>
+            <span className="text">submit</span>
+            <span className="bottom-key-1"></span>
+            <span className="bottom-key-2"></span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
