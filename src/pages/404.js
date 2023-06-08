692e7f4e2854c1dc061cb2df70bedbf93261ff8f
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-08 13:05:37 -0500

    change page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,31 @@
-import Layout from "@/components/Layout";
 import Link from "next/link";
 
 export default function custom404() {
   return (
-    <Layout>
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-center">
-        <h1>Error 404 !!</h1>
-        <p>
-          Esta página no existe. Favor de redirigirse al 
-          <Link href="/">
-            <button> Home</button>
-          </Link>
-        </p>
+    <section className="page_404 mx-auto items-center">
+      <div className="container mx-auto">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <div className="four_zero_four_bg">
+                <h1 className="text-center text-custom-azul">Error 404!!</h1>
+              </div>
+
+              <div className="contant_box_404">
+                <h3 className="h2 text-custom-azul">Parece que estás perdido</h3>
+
+                <p className="text-custom-azul">¡La página que buscas no existe!</p>
+
+                <Link href="/">
+                  <button className="link_404">
+                    Ir al Inicio
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </Layout>
-  
+    </section>
   );
 }
