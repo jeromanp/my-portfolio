de064997b73501dea7491b9d88556e619fdd8b85
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-08 20:38:09 -0500

    z-index

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-<nav className="pb-4 flex justify-end pr-5 pt-5 bg-custom-azul border border-custom-oro fixed top1 w-full">
+<nav className="z-50 pb-4 flex justify-end pr-5 pt-5 bg-custom-azul border border-custom-oro fixed top1 w-full">
     <ul className="flex text-lg font-semibold self-center">
     <li className="mr-5 hover:text-red-700">
       <Link href="#intro">Inicio</Link>
