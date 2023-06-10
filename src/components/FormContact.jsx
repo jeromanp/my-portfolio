4faf4afcc3f930cec31101954604e6f477bb8a8b
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-10 02:33:49 -0500

    design responsive

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -3,14 +3,18 @@ import { contactoInfo } from "@/utils/data";
 
 export default function FormContact() {
   return (
-    <div className="w-90 max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto">
-      <div className="p-20">
-        <h1 className="text-4xl font-bold mb-4 text-custom-oro">Contacto</h1>
-        <p className="mb-2 text-gray-500 text-justify text-xl">
-          {contactoInfo.parrafo_1}
-        </p>
+    <div className="w-full max-w-screen-xl mx-auto items-center justify-between font-mono text-sm bg-custom-azul">
+      <div className="p-5 sm:p-7 md:p-10 lg:p-20 xl:p-20">
+        <h1 className="text-2xl sm:text-3x1 md:text-4x1 lg:text-4x1 font-bold mb-4 text-custom-oro_2">
+          Contacto
+        </h1>
+        <div className="text-xs md:text-lg lg:text-xl">
+          <p className="mb-2 text-gray-500 text-justify">
+            {contactoInfo.parrafo_1}
+          </p>
+        </div>
 
-        <form className="form mx-auto mt-20">
+        <form className="form mx-auto mt-5 md:mt-10 lg:mt-20">
           <div className="flexy">
             <label>
               <input
@@ -48,7 +52,7 @@ export default function FormContact() {
             ></textarea>
           </label>
 
-          <button className="fancy" href="#">
+          <button className="fancy" href="/">
             <span className="top-key"></span>
             <span className="text">submit</span>
             <span className="bottom-key-1"></span>
