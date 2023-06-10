0fbdbe415069e4c53163b02c65cfab7df8d13d95
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-10 00:32:50 -0500

    design responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,10 +2,9 @@ import { aboutMe } from "@/utils/data";
 
 export default function AboutMe() {
   return (
-    // <div className="w-full min-h-screen flex flex-row mx-auto p-5 bg-green-800">
-    <div className="w-90 max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto">
-      <div className="p-20">
-        <h1 className="text-4xl font-bold mb-4 text-custom-oro_2">Sobre mí</h1>
+    <div className="w-full max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto">
+      <div className="sm:p-10 md:p-10 lg:p-20 xl:p-20">
+        <h1 className="text-4xl font-bold mb-4 text-custom-oro_2 sm:text-center md:text-start">Sobre mí</h1>
         <div className="text-xl">
           <p className="mb-2  text-gray-400 text-justify">{aboutMe.intro}</p>
           <p className="mb-2  text-gray-400 text-justify ">{aboutMe.estudio}</p>
