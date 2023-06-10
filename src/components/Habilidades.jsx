a0df7af00e0a4052d1fa15e486c85e1588a2b07c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-10 02:09:05 -0500

    design responsive

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -3,33 +3,31 @@ import { habilities, pHabilidades } from "@/utils/data";
 
 export default function Habilidades() {
   return (
-    // <div className="w-full min-h-screen flex flex-row mx-auto p-5 bg-red-800">
-    <div className="w-90 max-w-screen-xl items-center justify-between font-mono text-sm mx-auto bg-custom-azul">
-      <div className="p-20">
+    <div className="w-full max-w-screen-xl mx-auto items-center justify-between font-mono text-sm  bg-custom-azul">
+      <div className="p-5 sm:p-5 md:p-10 lg:p-20 xl:p-20">
         <h1 className="text-4xl font-bold mb-4 text-custom-oro_2">
           Habilidades
         </h1>
 
-        <div className="text-xl">
+        <div className="text-xs md:text-lg lg:text-xl">
           <p className="mb-2  text-gray-400 text-justify">{pHabilidades.uno}</p>
         </div>
+
         <div className="w-90vw mx-auto p-10">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-center py-8 font-bold dark:font-normal aos-init aos-animate">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 text-center py-8 font-bold dark:font-normal aos-init aos-animate">
             {habilities.map((hability, i) => (
               <div
-                className="bg-slate-200 dark:bg-[#928652] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-500 items-center inline-block"
+                className="bg-slate-200 dark:bg-[#928652] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-200 items-center inline-block"
                 key={i}
-                style={{ width: "100%", maxWidth: "200px", height: "auto" }}
               >
-                <div className="p-1 items-center inline-block">
+                <div className="p-1 items-center inline-block max-w-50 h-auto md:max-w-200 md:h-auto">
                   <img
                     src={hability.image}
                     alt={hability.skill}
-                    className="w-48 h-48"
                   />
                 </div>
                 <p
-                  className="mt-4 text-xl pb-5 text-center"
+                  className="text-md sm:text-lg md:text-lg lg:text-xl mt-4  pb-5 text-center"
                   style={{ wordWrap: "break-word" }}
                 >
                   {hability.skill}
