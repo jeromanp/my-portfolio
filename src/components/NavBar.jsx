75375acd3e8b40a392343ff40a205103808d539c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-10 21:31:34 -0500

    add logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,13 +36,13 @@ export default function Example() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    src="/imagen/logo.jpg"
+                    alt="logo"
                   />
                   <img
                     className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    src="/imagen/logo.jpg"
+                    alt="logo"
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
