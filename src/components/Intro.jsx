377019ecc05bf3e8a1bf39d68a19a05d9003beae
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-11 23:23:06 -0500

    add styles responsive

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,17 +4,19 @@ import React from "react";
 export default function Intro() {
   return (
     <div className="w-full max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto pt-10 md:pt-20 md:pb-10">
-      <h1 className="text-2xl sm:text-3x1 md:text-4x1 lg:text-4x1  font-bold text-custom-oro_2 text-center">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl  font-bold text-custom-oro_2 text-center">
         {infoPersonal.name}
       </h1>
-      <div className="text-xs md:text-lg lg:text-xl">
-        <p className="text-center mt-5 text-gray-400">
-          {infoPersonal.professionOne} <br />
+      <div className="text-sm md:text-xl lg:text-2xl">
+        <p className="text-center mt-4 md:mt-9 text-gray-400">
+          {infoPersonal.professionOne}
+        </p>
+        <p className="text-center mt-4 text-gray-400">
           {infoPersonal.professionTwo}
         </p>
       </div>
 
-      <div className="w-20 h-20 md:w-60 sm:h-60 sm:w-70 md:h-70 mx-auto flex justify-center items-center">
+      <div className="w-20 h-auto md:w-60 lg:w-80 mx-auto flex justify-center items-center">
         <div className="w-full h-full flex justify-center items-center">
           <img
             src="/imagen/img-me.jpg"
