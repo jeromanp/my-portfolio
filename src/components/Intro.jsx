8ac4715bd0f03c72619d72e27654c6823129404d
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-11 23:28:25 -0500

    animation image

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Intro() {
   return (
     <div className="w-full max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto pt-10 md:pt-20 md:pb-10">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl  font-bold text-custom-oro_2 text-center">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-custom-oro_2 text-center">
         {infoPersonal.name}
       </h1>
       <div className="text-sm md:text-xl lg:text-2xl">
@@ -17,11 +17,11 @@ export default function Intro() {
       </div>
 
       <div className="w-20 h-auto md:w-60 lg:w-80 mx-auto flex justify-center items-center">
-        <div className="w-full h-full flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center animate-pulse">
           <img
             src="/imagen/img-me.jpg"
             alt="me"
-            className="object-cover rounded-full max-w-full max-h-full pt-2"
+            className="object-cover rounded-full max-w-full max-h-full pt-2 shadow-lg"
           />
         </div>
       </div>
