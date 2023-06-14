a343887ff13d8579bd3168b929fe803128956863
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-13 20:27:46 -0500

    add animations

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -5,11 +5,11 @@ export default function Habilidades() {
   return (
     <div className="w-full max-w-screen-xl mx-auto items-center justify-between font-mono text-sm  bg-custom-azul">
       <div className="p-5 sm:p-5 md:p-10 lg:p-20 xl:p-20">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-4 text-custom-oro_2">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-4 text-custom-oro_2 animate-fadeInLeft">
           Habilidades
         </h1>
 
-        <div className="text-xs md:text-lg lg:text-xl">
+        <div className="text-xs md:text-lg lg:text-xl animate-fadeInDown">
           <p className="mb-2  text-gray-400 text-justify">{pHabilidades.uno}</p>
         </div>
 
@@ -17,10 +17,10 @@ export default function Habilidades() {
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 text-center py-8 font-bold dark:font-normal aos-init aos-animate">
             {habilities.map((hability, i) => (
               <div
-                className="bg-slate-200 dark:bg-[#928652] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-200 items-center inline-block"
+                className="bg-slate-200 dark:bg-[#928652] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-200 items-center inline-block animate-tada"
                 key={i}
               >
-                <div className="p-1 items-center inline-block max-w-50 h-auto md:max-w-200 md:h-auto shadow-2xl shadow-custom-azul">
+                <div className="p-1 items-center inline-block max-w-50 h-auto md:max-w-200 md:h-auto shadow-2xl shadow-custom-azul ">
                   <img
                     src={hability.image}
                     alt={hability.skill}
