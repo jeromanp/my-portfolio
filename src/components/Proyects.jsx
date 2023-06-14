e5e4bef960959f3d0ccb88b0424d467082b2034b
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-06-13 20:37:01 -0500

    add animations

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -5,10 +5,10 @@ export default function Proyects() {
   return (
     <div className="z-0 w-90 max-w-screen-xl items-center justify-between font-mono text-sm bg-custom-azul mx-auto">
       <div className="p-5 sm:p-5 md:p-10 lg:p-20 xl:p-20">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-4 text-custom-oro">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-4 text-custom-oro animate-fadeInLeft">
           Proyectos
         </h1>
-        <div className="text-xs md:text-lg lg:text-xl">
+        <div className="text-xs md:text-lg lg:text-xl animate-fadeInDown">
           <p className="mb-2 text-gray-500 text-justify">
             {proyectos.parrafo_1}
           </p>
@@ -24,7 +24,7 @@ export default function Proyects() {
               key={i}
             >
               <div className="md:w-1/2 sm:w-[150%] xs:w-[150%]">
-                <div className="mb-3 p-2 sm:p-2 md:p-0 lg:p-5">
+                <div className="mb-3 p-2 sm:p-2 md:p-0 lg:p-5 animate-fadeInUp">
                   <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl font-bold mb-4 font-bold mb-4 cursor-text text-white">
                     {proyect.title}
                   </h1>
@@ -40,7 +40,7 @@ export default function Proyects() {
                   </div>
                 </div>
 
-                <div className="items-center">
+                <div className="items-center animate-fadeOut">
                   <div className="mb-4 flex justify-center space-x-2">
                     <Link href={proyect.deploy} target="_blank">
                       <button
@@ -64,7 +64,7 @@ export default function Proyects() {
               </div>
 
               {proyect.video ? (
-                <div className="sm:w-[100%] md:w-1/2 mx-auto items-center z-0">
+                <div className="sm:w-[100%] md:w-1/2 mx-auto items-center z-0 animate-spinnerGrow">
                   <video className="w-full pb-10 pl-4 z-0" controls>
                     <source src={proyect.video} type="video/mp4" />
                     Tu navegador no soporta el elemento de video.
