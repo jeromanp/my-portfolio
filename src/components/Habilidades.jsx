294c4915f28dafebb3ebede660b16d9a86166111
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-01 12:03:25 -0600

    style: change style in component

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { habilities, pHabilidades } from "@/utils/data";
 import { useInView } from "react-intersection-observer";
 
-
 export default function Habilidades() {
   const [inViewRef, inView] = useInView({
     triggerOnce: true, // Solo activa la animación una vez
@@ -10,15 +9,20 @@ export default function Habilidades() {
   return (
     <div className="w-full mx-auto items-center justify-between font-mono text-sm  bg-custom-azul">
       <div className="p-5 sm:p-5 md:p-10 lg:p-20 xl:p-20">
-        <h1 className="nothing text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-4 text-custom-oro_2">
+        <h1
+          ref={inViewRef}
+          className={`nothing text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold mb-4 text-custom-oro_2  ${
+            inView ? "animate__animated animate__zoomInDown" : ""
+          }`}
+        >
           Habilidades
         </h1>
         <div
-        ref={inViewRef}
-        className={`  ${inView ? "animate__animated animate__fadeInTopRight" : ""}`}
-      >
-  
-        <div className="quick text-xs md:text-lg lg:text-xl ">
+          ref={inViewRef}
+          className={`quick text-xs md:text-lg lg:text-xl  ${
+            inView ? "animate__animated animate__jackInTheBox" : ""
+          }`}
+        >
           <p className="mb-2  text-gray-400 text-justify">{pHabilidades.uno}</p>
         </div>
 
@@ -26,14 +30,13 @@ export default function Habilidades() {
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 text-center py-8 font-bold dark:font-normal">
             {habilities.map((hability, i) => (
               <div
-                className="bg-slate-200 dark:bg-[#928652] shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-200 items-center inline-block hover:animate-pulse"
-                key={i}
-              >
+              ref={inViewRef}
+              className={`shadow-md shadow-[#040c16] py-2 hover:scale-110 duration-200 items-center inline-block hover:animate-pulse ${inView ? "animate__animated animate__rollIn" : ""}`}
+              key={i}
+            >
+                
                 <div className="p-1 items-center inline-block max-w-50 h-auto md:max-w-200 md:h-auto shadow-2xl shadow-custom-azul ">
-                  <img
-                    src={hability.image}
-                    alt={hability.skill}
-                  />
+                  <img style={{width:"15vh"}} src={hability.image} alt={hability.skill} />
                 </div>
                 <p
                   className="text-md sm:text-lg md:text-lg lg:text-xl mt-4  pb-5 text-center "
@@ -45,9 +48,6 @@ export default function Habilidades() {
             ))}
           </div>
         </div>
-        </div>
-
-
       </div>
     </div>
   );
