d9e9834817983b9d717ccd9c4f72c63f1498e2ac
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-01 11:22:08 -0600

    style: add animation

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,10 +9,14 @@ export default function AboutMe() {
   return (
     <div className="w-full items-center justify-between font-mono text-sm bg-custom-azul mx-auto">
       <div className="p-5 sm:p-5 md:p-10 lg:p-20 xl:p-20">
-        <h1 className="nothing text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-4 text-custom-oro_2 ">Sobre mí</h1>
+      <h1
+        ref={inViewRef}
+        className={`nothing text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold mb-4 text-custom-oro_2  ${inView ? "animate__animated animate__zoomInDown" : ""}`}
+      >Sobre mí</h1>
+        
         <div
         ref={inViewRef}
-        className={`quick text-xs md:text-lg lg:text-xl whitespace-pre-line  ${inView ? "animate__animated animate__fadeInTopLeft" : ""}`}
+        className={`quick text-xs md:text-lg lg:text-xl whitespace-pre-line  ${inView ? "animate__animated animate__jackInTheBox" : ""}`}
       >
           <p className="mb-2  text-gray-400 text-justify">{aboutMe.intro}</p>
           <p className="mb-2  text-gray-400 text-justify ">{aboutMe.estudio}</p>
