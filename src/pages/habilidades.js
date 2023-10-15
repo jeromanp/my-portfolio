65938d2ef13f470d720a390218e51e4a998a44dd
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-10-14 23:29:10 -0600

    create page habilidades

---
+++ src/pages/habilidades.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "@/components/Layout";
+import Habilidades from "@/components/Habilidades";
+
+export default function habilidades() {
+  return (
+    <Layout>
+      <Habilidades />
+    </Layout>
+  );
+}
