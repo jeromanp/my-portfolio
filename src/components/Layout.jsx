4d348237f643529890309ede3ff004c8bb45f876
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-10-14 22:32:46 -0600

    add metadata

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,16 +9,31 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Web Portfolio Jose Eduardo Roman"
+          content="Hola soy Jose Eduardo Roman, Desarrollador Fullstack con background en Redes y Telecomunicaciones"
         ></meta>
-        <link rel="icon" href="/imagen/logo.jpg" />
+        <link rel="icon" href="/imagen/logo3.png" />
         <meta name="author" content="Jose Eduardo Roman Piña" />
-        
+
         {/* Metadatos para redes sociales */}
-        <meta property="og:title" content="Jose Eduardo Roman" />
-        <meta property="og:description" content="Web Portfolio Jose Eduardo Roman" />
-        <meta property="og:image" content="/imagen/logo.jpg" /> 
-        <meta property="og:url" content="https://jeromanpi.vercel.app/" /> 
+        <meta
+          property="og:title"
+          content="Jose Eduardo Roman - Desarrollador Fullstack"
+        />
+        <meta
+          property="og:description"
+          content="Hola soy Jose Eduardo Roman, Desarrollador Fullstack con background en Redes y Telecomunicaciones"
+        />
+        <meta property="og:image" content="/imagen/logo3.png" />
+        <meta property="og:url" content="https://www.jeromanp.website" />
+        <meta
+          name="twitter:title"
+          content="Jose Eduardo Roman - Desarrollador Fullstack"
+        />
+        <meta
+          name="twitter:description"
+          content="Hola soy Jose Eduardo Roman, Desarrollador Fullstack con background en Redes y Telecomunicaciones"
+        />
+        <meta name="twitter:image" content="/imagen/logo3.png" />
       </Head>
       <NavBar />
 
