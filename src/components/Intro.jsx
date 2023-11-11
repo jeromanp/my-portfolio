a35313427037bc86a34f3196c7f287014f6b0a8e
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-11-11 01:16:54 -0600

    responsive index

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,20 +11,20 @@ export default function Intro() {
       {/* ////////////////////// */}
       <section>
         <div className="bg-custom-azul text-white py-10">
-          <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+          <div className="container mx-auto flex flex-col lg:flex-row items-center my-2 md:my-20">
             <div
               ref={inViewRef}
-              className={`flex flex-col w-full lg:w-1/3 justify-center items-start ${
+              className={`flex flex-col w-full lg:w-1/3 justify-center items-center mx-20 ${
                 inView ? "animate__animated animate__backInDown" : ""
               }`}
             >
-              <h1 className="text-3xl md:text-6xl p-2 text-custom-oro_2 tracking-loose my-5">
+              <h1 className="text-3xl md:text-6xl p-2 text-custom-oro_2 tracking-loose">
                 Hola!!{" "}
               </h1>
-              <h2 className="text-xl md:text-3xl lg:text-5xl leading-relaxed md:leading-snug mb-2 mb-5">
+              <h2 className="text-xl md:text-3xl lg:text-5xl leading-relaxed md:leading-snug mb-2">
                 Soy {infoPersonal.name}
               </h2>
-              <p className="text-sm md:text-base lg:text-xl text-gray-50 mb-4">
+              <p className="text-sm md:text-base lg:text-xl text-gray-50 mb-4 flex text-center justify-center items-center">
                 {infoPersonal.professionOne} e {infoPersonal.professionTwo}
               </p>
               <a
@@ -34,7 +34,7 @@ export default function Intro() {
                 Contactame
               </a>
             </div>
-            <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
+            <div className="p-1 mt-1 mb-2 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
               <div className="h-48 flex flex-wrap content-center">
                 <div
                   ref={inViewRef}
@@ -43,8 +43,8 @@ export default function Intro() {
                   }`}
                 >
                   <img
-                    style={{ width: "30vh", height: "auto" }}
-                    className="mx-4 my-7 h-auto object-cover rounded-full max-w-full max-h-full pt-4 shadow-lg"
+                    // style={{ width: "30vh", height: "auto" }}
+                    className="mx-4 my-7 h-auto object-cover rounded-full max-w-full max-h-full pt-4 shadow-lg w-20 lg:w-48 md:w-40 hidden md:inline"
                     src="/imagen/hero2.PNG"
                     alt="Developer"
                   />
@@ -56,8 +56,8 @@ export default function Intro() {
                   }`}
                 >
                   <img
-                    style={{ width: "35vh", height: "auto" }}
-                    className="h-auto object-cover rounded-full max-w-full max-h-full pt-4 shadow-lg -my-20"
+                    // style={{ width: "35vh", height: "auto" }}
+                    className="h-auto object-cover rounded-full max-w-full max-h-full pt-4 lg:-my-20 w-24 lg:w-60 md:w-48 inline"
                     src="/imagen/img-me.jpg"
                     alt="JERP"
                   />
@@ -69,8 +69,8 @@ export default function Intro() {
                   }`}
                 >
                   <img
-                    style={{ width: "30vh", height: "auto" }}
-                    className="mx-4 my-7 h-auto object-cover rounded-full max-w-full max-h-full pt-4 shadow-lg"
+                    // style={{ width: "30vh", height: "auto" }}
+                    className="mx-4 my-7 h-auto object-cover rounded-full max-w-full max-h-full pt-4 shadow-lg w-24 lg:w-48 md:w-40 hidden md:inline"
                     src="/imagen/hero1.PNG"
                     alt="Telecom"
                   />
@@ -80,7 +80,7 @@ export default function Intro() {
           </div>
           <div
             ref={inViewRef}
-            className={`indie text-sm md:text-xl lg:text-2xl py-10 mx-12 ${
+            className={`indie text-sm md:text-xl lg:text-2xl md:py-10 mx-2 md:mx-12 ${
               inView ? "animate__animated animate__bounceInLeft" : ""
             }`}
           >
