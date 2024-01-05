2e1b648aeee83ced350f89f501d910ca6715fb96
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-01-05 01:08:45 -0600

    title page working

--- src/pages/working.js
+++ src/pages/working.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '@/components/Layout'
 import Process from '@/components/Process'
+import Head from 'next/head'
 
 export default function Working() {
   return (
       <main>
+        <Head>
+        <title>Jose Eduardo Roman 💻📡 | Trabajando</title>
+      </Head>
       <Process />        
       </main>
   )
