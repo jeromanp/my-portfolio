6e9fd5a8a49babb3da415fd5c0a47ec7c09e27de
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-01-05 01:04:59 -0600

    contacto title

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Layout from "@/components/Layout";
 import ContactForm from "@/components/FormContact";
+import Head from "next/head";
 
 export default function contacto() {
   return (
     <Layout>
+      <Head>
+      <title>Jose Eduardo Roman 💻📡 | Contacto</title>        
+        <meta
+          name="description"
+          content="Si tienes alguna pregunta, propuesta de trabajo o simplemente quieres saludar, no dudes en contactarme. Estaré encantado de escucharte y responder lo antes posible."
+        ></meta>
+      </Head>
       <ContactForm />
     </Layout>
   );
