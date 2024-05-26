2d99cf0f14597e908db703eaef2bdc89ee1778f2
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-25 21:56:00 -0600

    responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import Link from "next/link";
+import { useInView } from "react-intersection-observer";
 
 export default function About() {
+  const [inViewRef, inView] = useInView({
+    triggerOnce: true, // Solo activa la animación una vez
+  });
   return (
     <div className="bg-custom-oro_3">
-      <div className="container py-10 mx-auto md:px-6">
-        <div className="self-stretch h-[574px] relative max-w-full text-left text-base text-blue font-text-small-link mq1350:h-auto mq1350:min-h-[574]">
-          <div className="absolute top-[0px] left-[0px] bg-custom-oro_3 w-full overflow-hidden flex flex-col items-start justify-start py-28 px-16 box-border max-w-full h-full mq800:gap-[40px] mq800:pl-8 mq800:pr-8 mq800:box-border mq450:gap-[20px]">
+      <div className="container py-5 lg:py-10 mx-auto md:px-6">
+        <div className="mx-auto h-auto self-stretch relative max-w-full text-left text-base text-blue font-text-small-link flex flex-col lg:flex-row">
+          <div className="bg-custom-oro_3 w-full flex flex-col items-start justify-start py-2 lg:py-10 px-16 box-border max-w-full h-full mq800:gap-[40px] mq800:pl-8 mq800:pr-8 mq800:box-border mq450:gap-[5px]">
             <div className="self-stretch flex flex-row flex-wrap items-start justify-start gap-[80px] max-w-full mq800:gap-[40px] mq450:gap-[20px]">
-              <div className="flex-1 overflow-hidden flex flex-col items-start justify-start gap-[16px] min-w-[400px] max-w-full mq800:min-w-full">
-                <div className="self-stretch h-6 relative leading-[150%] font-semibold hidden" />
+              <div className="flex-1 flex flex-col items-start justify-start gap-[16px] min-w-[400px]">
                 <div className="self-stretch h-[116px] relative text-29xl font-h3 inline-block shrink-0 mq800:text-19xl mq450:text-10xl" />
               </div>
               <div className="flex-1 flex flex-col items-start justify-start py-6 px-0 box-border gap-[24px] min-w-[400px] max-w-full text-lg font-paragraph mq800:min-w-full">
-                <div className="self-stretch flex flex-col items-start justify-start gap-[32px] mq800:gap-[16px]">
-                  <div className="self-stretch relative text-custom-azul text-justify">
-                    <p className="m-0 ">
+                <div
+                  ref={inViewRef}
+                  className={`self-stretch flex flex-col items-start justify-start gap-[32px] mq800:gap-[16px] ${
+                    inView ? "animate__animated animate__backInDown" : ""
+                  }`}
+                >
+                  <div className="self-stretch relative text-custom-azul text-justify quick">
+                    <p className="m-0">
                       Soy Ingeniero en Telecomunicaciones, Sistemas y
                       Electrónica con especialización en Redes e
                       Interconectividad.
@@ -31,22 +39,16 @@ export default function About() {
                       permitan desarrollarme en el mundo de TI.
                     </p>
                   </div>
-                  <div className="self-stretch hidden flex-row items-start justify-start py-2 px-0 gap-[24px] text-xl font-h3">
-                    <div className="h-[116px] w-[296px] hidden flex-row items-start justify-start gap-[16px]">
-                      <img
-                        className="h-8 w-8 relative overflow-hidden shrink-0"
-                        alt=""
-                        src="/icon--relume.svg"
-                      />
-                    </div>
-                  </div>
                 </div>
                 <div className="flex flex-row items-center justify-start py-8 px-0 gap-[24px] text-base font-btn">
-                  <button className="cursor-pointer py-2.5 px-[23px] bg-[transparent] flex flex-row items-center justify-center border-[1px] border-solid border-black hover:box-border hover:border-[1px] hover:border-solid hover:bg-custom-oro hover:text-white">
-                    <div className="relative text-base font-btn text-custom-azul text-left inline-block min-w-[86px]">
-                      Contáctame
-                    </div>
-                  </button>
+                  <Link href="/contacto">
+                    <button className="cursor-pointer py-2.5 px-[23px] bg-[transparent] flex flex-row items-center justify-center border-[1px] border-solid border-black hover:box-border hover:border-[1px] hover:border-solid hover:bg-custom-oro hover:text-white">
+                      <div className="relative text-base font-btn text-custom-azul text-left inline-block min-w-[86px]">
+                        Contáctame
+                      </div>
+                    </button>
+                  </Link>
+
                   <div className="flex flex-row items-center justify-center gap-[8px]">
                     <div className="relative inline-block min-w-[59px]">
                       <Link href="/proyectos">
@@ -67,12 +69,19 @@ export default function About() {
               </div>
             </div>
           </div>
-          <img
-            className="absolute top-[139px] left-[245px] w-[246px] h-[305px] object-cover z-[1] "
-            loading="lazy"
-            alt=""
-            src="/imagen/img-me.jpg"
-          />
+          <div
+            ref={inViewRef}
+            className={`flex mx-auto items-center ${
+              inView ? "animate__animated animate__backInDown" : ""
+            }`}
+          >
+            <img
+              className="relative w-[246px] h-[305px] lg:w-[346px] lg:h-[405px] object-cover z-[1] hover:opacity-70"
+              loading="lazy"
+              alt=""
+              src="/imagen/img-me.jpg"
+            />
+          </div>
         </div>
       </div>
     </div>
