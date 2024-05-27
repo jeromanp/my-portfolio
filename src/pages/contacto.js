d69609a5d24bfcdf6f956ad8af83a167aaefcdf2
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-26 19:27:01 -0600

    page contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "@/components/Layout";
 import ContactForm from "@/components/FormContact";
 import Head from "next/head";
+import CTA from "@/components/CTA";
 
 export default function contacto() {
   return (
@@ -13,6 +14,7 @@ export default function contacto() {
           content="Si tienes alguna pregunta, propuesta de trabajo o simplemente quieres saludar, no dudes en contactarme. Estaré encantado de escucharte y responder lo antes posible."
         ></meta>
       </Head>
+      <CTA title="Comunicate" subtitle="conmigo!!" buttonText="Ver CV" href="https://drive.google.com/file/d/1WAqDToFDsIHQDofy4oa8iZR5e0v5wPx_/view"/>
       <ContactForm />
     </Layout>
   );
