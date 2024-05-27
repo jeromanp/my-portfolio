b794b5b22151d3f7d7fcdbd488b9f4fc32b79fb7
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-26 19:27:48 -0600

    descomment countVisits

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import CTA from "@/components/CTA";
 export default function Home() {
   return (
     <Layout>
-      {/* <CountVisit /> */}
+      <CountVisit />
       <Hero />
       <About />
       <Experence />
