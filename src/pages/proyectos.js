28f799e7c9dc2b09bd700888d4a83a610a970e32
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-26 19:26:53 -0600

    page proyectos

--- src/pages/proyectos.js
+++ src/pages/proyectos.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import Layout from '@/components/Layout'
-import Proyects from '@/components/Proyects'
-import Head from 'next/head'
+import React from "react";
+import Layout from "@/components/Layout";
+import Proyects from "@/components/Proyects";
+import Head from "next/head";
+import CTA from "@/components/CTA";
 
 export default function proyectos() {
   return (
     <Layout>
       <Head>
-      <title>Jose Eduardo Roman 💻📡 | Proyectos</title>        
+        <title>Jose Eduardo Roman 💻📡 | Proyectos</title>
         <meta
           name="description"
           content="A continuación presento los proyectos más destacados que he realizado desde que inicie esta etapa en el mundo del desarrollo web"
         ></meta>
       </Head>
-        <Proyects/>
+      <CTA title="La práctica hace al" subtitle="Maestro" buttonText="Ver CV" href="https://drive.google.com/file/d/1WAqDToFDsIHQDofy4oa8iZR5e0v5wPx_/view" />
+      <Proyects />
     </Layout>
-  )
+  );
 }
