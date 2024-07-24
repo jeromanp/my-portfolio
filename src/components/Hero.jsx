182bed4a8bbf1c14dbca5852afa442cb876a9620
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-23 21:29:52 -0600

    resevrse animatoj cursor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
     triggerOnce: true, // Solo activa la animación una vez
   });
 
-  const [isActive, setIsActive] = useState(false);
+  // const [isActive, setIsActive] = useState(false);
 
   return (
     <section className="self-stretch overflow-hidden flex flex-row items-center justify-center py-[100px] px-5 box-border bg-[url('/imagen/hero.png')] bg-cover bg-no-repeat bg-[top] max-w-full z-[1] text-center text-37xl text-white font-h3 mq450:pt-[122px] mq450:pb-[122px] mq450:box-border mq1125:pt-[188px] mq1125:pb-[188px] mq1125:box-border">
@@ -22,12 +22,12 @@ export default function Hero() {
         >
           <div>
             <h1
-              onMouseOver={() => {
-                setIsActive(true);
-              }}
-              onMouseLeave={() => {
-                setIsActive(false);
-              }}
+              // onMouseOver={() => {
+              //   setIsActive(true);
+              // }}
+              // onMouseLeave={() => {
+              //   setIsActive(false);
+              // }}
               className="m-0 self-stretch relative text-inherit font-h3 font-inherit mq800:text-26xl mq450:text-15xl indie text-4xl lg:text-6xl"
             >
               ¡Bienvenido a mi portafolio personal!
@@ -36,7 +36,7 @@ export default function Hero() {
               Soy Jose Eduardo Roman, un desarrollador web Fullstack apasionado
               por crear soluciones digitales innovadoras.
             </div>
-            <GradientCursor isActive={isActive} />
+            {/* <GradientCursor isActive={isActive} /> */}
           </div>
         </div>
         <div
