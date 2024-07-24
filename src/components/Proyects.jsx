f996464be652e5cade3dd6a1059ae23e34c0602b
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-23 22:00:12 -0600

    apply animation

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -3,6 +3,7 @@ import { proyectos, misProyectos } from "@/utils/data";
 import Link from "next/link";
 import Modal from "@/components/Modal";
 import { useInView } from "react-intersection-observer";
+import BlazierCurve from "./animations.jsx/BlazierCurve";
 
 export default function Proyects() {
   const [inViewRef, inView] = useInView({
@@ -42,114 +43,117 @@ export default function Proyects() {
           }`}
         >
           {misProyectos.map((proyect, i) => (
-            <div
-              className="mx-auto container flex flex-col sm:flex-rom md:flex-row items-center justify-center h-full p-1 sm:p-5 md:p-10 lg:p-10"
-              key={i}
-            >
+            <div>
               <div
-                id={proyect.title}
-                className="md:w-1/2 sm:w-[150%] xs:w-[150%]"
+                className="mx-auto container flex flex-col sm:flex-rom md:flex-row items-center justify-center h-full p-1 sm:p-5 md:p-10 lg:p-10"
+                key={i}
               >
-                <div className="mb-2 p-2 sm:p-2 md:p-0 lg:p-5 animate-fadeInUp">
-                  <h2 className="indie text-xl sm:text-2xl md:text-3xl lg:text-3xl font-bold mb-4 cursor-text text-custom-azul hover:text-custom-azul_2">
-                    {proyect.title}
-                  </h2>
+                <div
+                  id={proyect.title}
+                  className="md:w-1/2 sm:w-[150%] xs:w-[150%]"
+                >
+                  <div className="mb-2 p-2 sm:p-2 md:p-0 lg:p-5 animate-fadeInUp">
+                    <h2 className="indie text-xl sm:text-2xl md:text-3xl lg:text-3xl font-bold mb-4 cursor-text text-custom-azul hover:text-custom-azul_2">
+                      {proyect.title}
+                    </h2>
 
-                  <div className="quick text-xs sm: text-md md:text-md lg:text-xl">
-                    <br />
-                    <p className="cursor-text text-justify text-m whitespace-pre-line text-white-500">
-                      Tecnologías utilizadas:{" "}
-                      <span className="text-green-700 hover:text-green-900">
-                        {proyect.tecnologies}
-                      </span>
-                    </p>
-                    <p className="cursor-text text-justify text-m whitespace-pre-line text-white-500">
-                      Servicios de despliegue:{" "}
-                      <span className="text-red-700 hover:text-red-900">
-                        {proyect.production}
-                      </span>
-                    </p>
-                    <p>
-                      Visible para estos dispositivos:{" "}
-                      <span className="text-purple-700 hover:text-purple-900">
-                        {proyect.name_dispositive}
-                      </span>
-                      <img
-                        src={proyect.dispositive}
-                        alt="dispositivo"
-                        className=" ml-5 my-2 w-10 md:w-14 lg:w-16 h-auto hover:animate-bounce"
-                      />{" "}
-                    </p>
-                    <div className="pt-2 flex items-start">
-                      <Modal
-                        title={proyect.title}
-                        description={proyect.description}
-                      />
+                    <div className="quick text-xs sm: text-md md:text-md lg:text-xl">
+                      <br />
+                      <p className="cursor-text text-justify text-m whitespace-pre-line text-white-500">
+                        Tecnologías utilizadas:{" "}
+                        <span className="text-green-700 hover:text-green-900">
+                          {proyect.tecnologies}
+                        </span>
+                      </p>
+                      <p className="cursor-text text-justify text-m whitespace-pre-line text-white-500">
+                        Servicios de despliegue:{" "}
+                        <span className="text-red-700 hover:text-red-900">
+                          {proyect.production}
+                        </span>
+                      </p>
+                      <p>
+                        Visible para estos dispositivos:{" "}
+                        <span className="text-purple-700 hover:text-purple-900">
+                          {proyect.name_dispositive}
+                        </span>
+                        <img
+                          src={proyect.dispositive}
+                          alt="dispositivo"
+                          className=" ml-5 my-2 w-10 md:w-14 lg:w-16 h-auto hover:animate-bounce"
+                        />{" "}
+                      </p>
+                      <div className="pt-2 flex items-start">
+                        <Modal
+                          title={proyect.title}
+                          description={proyect.description}
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="items-center animate-fadeOut">
+                    <div className="mb-4 flex justify-center space-x-2">
+                      {proyect.deploy === "/working" ? (
+                        <Link href={proyect.deploy}>
+                          <button
+                            className="bg-custom-oro_3 mr-2 inline-block transition duration-300 ease-in-out text-dark-800 hover:bg-custom-azul cursor-pointer py-2 px-4 rounded-lg border border-custom-oro"
+                            rel="noreferrer"
+                          >
+                            <span className="text-sm">Deploy</span>
+                          </button>
+                        </Link>
+                      ) : (
+                        <Link href={proyect.deploy} target="_blank">
+                          <button
+                            className="bg-custom-azul mr-2 inline-block transition duration-300 ease-in-out text-dark-800 hover:bg-custom-oro cursor-pointer py-2 px-4 rounded-lg border border-custom-oro"
+                            rel="noreferrer"
+                          >
+                            <span className="text-sm">Deploy</span>
+                          </button>
+                        </Link>
+                      )}
+
+                      <Link href={proyect.repository} target="_blank">
+                        <button
+                          className="bg-custom-oro mr-2 inline-block transition duration-300 ease-in-out text-dark-800 hover:bg-custom-azul cursor-pointer py-2 px-4 rounded-lg border border-custom-oro"
+                          rel="noreferrer"
+                        >
+                          <span className="text-sm">Repositorio</span>
+                        </button>
+                      </Link>
                     </div>
                   </div>
                 </div>
 
-                <div className="items-center animate-fadeOut">
-                  <div className="mb-4 flex justify-center space-x-2">
-                    {proyect.deploy === "/working" ? (
-                      <Link href={proyect.deploy}>
-                        <button
-                          className="bg-custom-oro_3 mr-2 inline-block transition duration-300 ease-in-out text-dark-800 hover:bg-custom-azul cursor-pointer py-2 px-4 rounded-lg border border-custom-oro"
-                          rel="noreferrer"
-                        >
-                          <span className="text-sm">Deploy</span>
-                        </button>
-                      </Link>
-                    ) : (
-                      <Link href={proyect.deploy} target="_blank">
-                        <button
-                          className="bg-custom-azul mr-2 inline-block transition duration-300 ease-in-out text-dark-800 hover:bg-custom-oro cursor-pointer py-2 px-4 rounded-lg border border-custom-oro"
-                          rel="noreferrer"
-                        >
-                          <span className="text-sm">Deploy</span>
-                        </button>
-                      </Link>
-                    )}
-
-                    <Link href={proyect.repository} target="_blank">
-                      <button
-                        className="bg-custom-oro mr-2 inline-block transition duration-300 ease-in-out text-dark-800 hover:bg-custom-azul cursor-pointer py-2 px-4 rounded-lg border border-custom-oro"
-                        rel="noreferrer"
-                      >
-                        <span className="text-sm">Repositorio</span>
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-
-              {proyect.video ? (
-                <div className="sm:w-[100%] md:w-1/2 mx-auto items-center z-0 animate-spinnerGrow">
-                  {/* <video className="w-full pb-10 pl-4 z-0" controls>
+                {proyect.video ? (
+                  <div className="sm:w-[100%] md:w-1/2 mx-auto items-center z-0 animate-spinnerGrow">
+                    {/* <video className="w-full pb-10 pl-4 z-0" controls>
                     <source src="https://www.youtube.com/watch?v=q2AvcqIsRt4" />
                     Tu navegador no soporta el elemento de video.
                   </video> */}
-                  <iframe
-                    // width="560"
-                    height="315"
-                    src={proyect.video}
-                    title={proyect.name}
-                    frameborder="0"
-                    allowfullscreen
-                    className="w-[100%] pb-10 pl-4 z-0"
-                  ></iframe>
-                </div>
-              ) : null}
+                    <iframe
+                      // width="560"
+                      height="315"
+                      src={proyect.video}
+                      title={proyect.name}
+                      frameborder="0"
+                      allowfullscreen
+                      className="w-[100%] pb-10 pl-4 z-0"
+                    ></iframe>
+                  </div>
+                ) : null}
 
-              {proyect.image ? (
-                <div className="sm:w-[100%] md:w-1/2 mx-auto items-center z-0 animate-spinnerGrow">
-                  <img
-                    src={proyect.image}
-                    alt={proyect.title}
-                    className="p-4"
-                  />
-                </div>
-              ) : null}
+                {proyect.image ? (
+                  <div className="sm:w-[100%] md:w-1/2 mx-auto items-center z-0 animate-spinnerGrow">
+                    <img
+                      src={proyect.image}
+                      alt={proyect.title}
+                      className="p-4"
+                    />
+                  </div>
+                ) : null}
+              </div>
+              <BlazierCurve />
             </div>
           ))}
         </div>
