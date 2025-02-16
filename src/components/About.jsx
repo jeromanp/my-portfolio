42a4fd850413e05d2763304a5ef42d6d292694c2
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2025-02-15 22:27:01 -0600

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,7 +61,7 @@ export default function About() {
                 >
                   <Link href="/contacto/#email">
                     <button className="cursor-pointer py-2.5 px-[23px] bg-[transparent] flex flex-row items-center justify-center border-[1px] border-solid border-black hover:box-border hover:border-[1px] hover:border-solid hover:bg-custom-oro hover:text-white">
-                      <div className="relative text-base font-btn text-custom-azul text-left inline-block min-w-[86px]">
+                      <div className="flex justify-center relative text-base font-btn text-custom-azul text-left min-w-[86px]">
                         Contacto
                       </div>
                     </button>
